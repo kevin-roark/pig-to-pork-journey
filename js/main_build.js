9bb102ef0f4141388f31e1849901159cede6145b
Kevin Roark <ker2143@columbia.edu>
2014-06-13 23:48:47 -0700

    phaser beginning

--- js/main_build.js
+++ js/main_build.js
@@ -122,14 +122,42 @@ function secondFlash() {
 
 var intro = require('./intro');
 
+var GAME_WIDTH = 800;
+var GAME_HEIGHT = 600;
+
 var $body = $('body');
+var game, pig, cannon;
+
 
 intro(function() {
-  postIntro();
+  game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'pork-journey', {preload: preload, create: createGame});
 });
 
-function postIntro() {
-  $body.html('more soon');
+function preload() {
+  game.load.image('flypig', 'assets/flypig.png');
+  game.load.image('cannon', 'assets/cannon.png');
+}
+
+function createGame() {
+  pig = game.add.sprite(50, GAME_HEIGHT - 200, 'flypig');
+  game.physics.enable(pig, Phaser.Physics.ARCADE);
+
+  cannon = game.add.sprite(0, GAME_HEIGHT - 200, 'cannon');
+  game.physics.enable(cannon, Phaser.Physics.ARCADE);
+
+  launchPig();
+}
+
+function launchPig() {
+  pig.body.velocity.x = 100;
+  pig.body.velocity.y = -40;
+
+  var launchInterval = setInterval(function() {
+    pig.body.velocity.y += 1;
+    if (pig.body.velocity.y >= 0) {
+      clearInterval(launchInterval);
+    }
+  }, 100);
 }
 
 },{"./intro":1}]},{},[2])
