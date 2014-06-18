f33c5a4201cad9901ed1ef30a85c78ea0f3739ec
Kevin Roark <ker2143@columbia.edu>
2014-06-17 20:05:59 -0700

    noticing intersections

--- js/main_build.js
+++ js/main_build.js
@@ -125,20 +125,23 @@ var intro = require('./intro');
 var GAME_WIDTH = 800;
 var GAME_HEIGHT = 600;
 
+var SPEED_DIFF = 50;
+
 var $body = $('body');
 var game, pig, cannon;
 var donuts = [];
 var keys;
+var controllable = false;
 
 
-intro(function() {
+//intro(function() {
   game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, 'pork-journey',
                          {preload: preload,
                           create: createGame,
                           update: update,
                           render: render
                          });
-});
+//});
 
 function preload() {
   game.load.image('flypig', 'assets/flypig.png');
@@ -170,18 +173,31 @@ function createGame() {
 function update() {
   for (var i = 0; i < donuts.length; i++) {
     var donut = donuts[i];
-    game.physics.arcade.collide(pig, donut);
+    var intersecting = game.physics.arcade.intersects(pig, donut);
+    if (intersecting) {
+      console.log('they touching');
+    }
   }
+
+  if (!controllable) return;
+ 
+  setPigMotion();
+ }
+
+function setPigMotion() {
+  pig.body.velocity.x = 0;
+  pig.body.velocity.y = 0;
 
   if (keys.left.isDown) {
-
+    pig.body.velocity.x -= SPEED_DIFF;
   } else if (keys.right.isDown) {
-
+    pig.body.velocity.x += SPEED_DIFF;
   } else if (keys.up.isDown) {
-
+    pig.body.velocity.y -= SPEED_DIFF;
   } else if (keys.down.isDown) {
-
+    pig.body.velocity.y += SPEED_DIFF;
   }
+
 }
 
 function render() {
@@ -196,6 +212,8 @@ function launchPig() {
     pig.body.velocity.y += 1;
     if (pig.body.velocity.y >= 0) {
       clearInterval(launchInterval);
+      pig.body.velocity.x = 0;
+      controllable = true;
     }
   }, 100);
 }
@@ -205,6 +223,7 @@ function createDonut() {
   donuts.push(donut);
   donut.name = 'donut' + donuts.length;
   game.physics.enable(donut, Phaser.Physics.ARCADE);
+  //donut.body.immovable = true;
 }
 
 
